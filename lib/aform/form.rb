3a6086dfd99e5f37c81edf1d103b3e8306278516
Anton Versal <ant.ver@gmail.com>
2014-06-29 14:55:10 +0300

    save validation methods

--- lib/aform/form.rb
+++ lib/aform/form.rb
@@ -1,11 +1,21 @@
 module Aform
   class Form
     class_attribute :params
+    class_attribute :validations
     self.params = []
+    self.validations = []
 
     class << self
       def param(*args)
         self.params += args
+      end
+
+      def method_missing(meth, *args, &block)
+        if meth.to_s.start_with?("validate")
+          self.validations << {method: meth, options: args}
+        else
+          super
+        end
       end
     end
   end
--- test/form_test.rb
+++ test/form_test.rb
@@ -1,18 +1,38 @@
 require 'test_helper'
 
-
 describe Aform::Form do
+
   describe ".param" do
-    class FooForm < Aform::Form
-      param :name, :count
-      param :size
+    subject do
+      Class.new(Aform::Form) do
+        param :name, :count
+        param :size
+      end.new
     end
 
-    let(:foo) { FooForm.new }
-    let(:bar) { BarForm.new }
-
     it "stores params" do
-      foo.params.must_equal [:name, :count, :size]
+      subject.params.must_equal([:name, :count, :size])
+    end
+  end
+
+  describe "validation remembering" do
+
+    subject do
+      Class.new(Aform::Form) do
+        param :name, :count
+        validates_presence_of :name
+        validates :count, presence: true, inclusion: [1..100]
+        validate :custom_validation
+      end.new
+    end
+
+    it "saves validation" do
+      validations = [
+        {method: :validates_presence_of, options: [:name]},
+        {method: :validates, options: [:count, {presence: true, inclusion: [1..100]}]},
+        {method: :validate, options: [:custom_validation]}
+      ]
+      subject.validations.must_equal(validations)
     end
   end
 end
