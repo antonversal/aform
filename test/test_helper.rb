c5b5a87d42425538c381f5f5a0b8a0ec1a32e372
Anton Versal <ant.ver@gmail.com>
2014-07-01 17:31:52 +0300

    `context` for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,13 @@ require 'aform'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
+require 'minitest/emoji'
 require 'pry'
+
+module MiniTest
+  class Spec
+    class << self
+      alias_method :context, :describe
+    end
+  end
+end
