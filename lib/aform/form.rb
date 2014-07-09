514699716e94c37af1c48e552debe1b628528780
Anton Versal <ant.ver@gmail.com>
2014-07-09 14:51:58 +0300

    added attribute name to nested forms

--- lib/aform/form.rb
+++ lib/aform/form.rb
@@ -6,16 +6,20 @@ module Aform
 
     attr_accessor :model, :attributes, :nested_forms
 
-    def initialize(ar_model, attributes, model_klass = Aform::Model, model_builder = Aform::Builder)
+    def initialize(ar_model, attributes, model_klass = Aform::Model,
+      model_builder = Aform::Builder)
+
       creator = model_builder.new(model_klass)
       self.model = creator.build_model_klass(self.params, self.validations).new(ar_model, attributes)
       self.attributes = attributes
       initialize_nested(ar_model, model_klass, model_builder)
     end
 
+    #TODO don't save all models if at leas one is fail
+
     def valid?
       if self.nested_forms
-        self.model.valid? && self.nested_forms.all?(&:valid?)
+        self.model.valid? && self.nested_forms.values.flatten.all?(&:valid?)
       else
         self.model.valid?
       end
@@ -23,10 +27,14 @@ module Aform
 
     def save
       if self.nested_forms
-        self.model.save && self.nested_forms.all?(&:save)
+        self.model.save && self.nested_forms.values.flatten.all?(&:save)
       else
         self.model.save
       end
+    end
+
+    def errors
+      self.model.errors.messages
     end
 
     class << self
@@ -67,9 +75,10 @@ module Aform
         nested_form_klasses.each do |k,v|
           if attributes.has_key? k
             attributes[k].each do |attrs|
-              self.nested_forms ||= []
+              self.nested_forms ||= {}
+              self.nested_forms[k] ||= []
               model = nested_ar_model(ar_model, k, attrs)
-              self.nested_forms << v.new(model, attrs, model_klass, model_builder)
+              self.nested_forms[k] << v.new(model, attrs, model_klass, model_builder)
             end
           end
         end
