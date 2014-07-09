14d9f5710f3a69a33504d0dc0c537d4ae9c1a653
Anton Versal <ant.ver@gmail.com>
2014-07-09 13:47:16 +0300

    updates records integration tests

--- test/integration/active_record_test.rb
+++ test/integration/active_record_test.rb
@@ -17,25 +17,48 @@ describe "ActiveRecord" do
     Post.delete_all
   end
 
-  describe "creating records" do
-    it "creates record" do
-      post = Post.new
-      attrs = {title: "Cool Post", author: "John Doe",
-               comments: [
-                 {message: "Great post man!", author: "Mr. Smith"},
-                 {message: "Really?", author: "Vasya"}
-               ]
-      }
-      form = PostForm.new(post,attrs)
-      form.save.must_equal true
-      Post.count.must_equal 1
-      Comment.count.must_equal 2
-      post = Post.first
-      post.title.must_equal("Cool Post")
-      post.author.must_equal("John Doe")
-      comments = post.comments
-      comments.map(&:message).must_equal(["Great post man!", "Really?"])
-      comments.map(&:author).must_equal(["Mr. Smith", "Vasya"])
-    end
+  it "creates records" do
+    post = Post.new
+    attrs = {title: "Cool Post", author: "John Doe",
+             comments: [
+               {message: "Great post man!", author: "Mr. Smith"},
+               {message: "Really?", author: "Vasya"}
+             ]
+    }
+    form = PostForm.new(post, attrs)
+    form.save.must_equal true
+    Post.count.must_equal 1
+    Comment.count.must_equal 2
+    post = Post.first
+    post.title.must_equal("Cool Post")
+    post.author.must_equal("John Doe")
+    comments = post.comments
+    comments.map(&:message).must_equal(["Great post man!", "Really?"])
+    comments.map(&:author).must_equal(["Mr. Smith", "Vasya"])
+  end
+
+  it "updates records" do
+    post = Post.create(title: "Cool Post", author: "John Doe")
+    comment = post.comments.create(message: "Great post man!", author: "Mr. Smith")
+
+    attrs = {title: "Very Cool Post", author: "John Doe",
+             comments: [
+               {id: comment.id, message: "Great post MAN!", author: "Mr. Smith"},
+               {message: "Really?", author: "Vasya"}
+             ]
+    }
+
+    post.reload
+    
+    form = PostForm.new(post, attrs)
+    form.save.must_equal true
+    Post.count.must_equal 1
+    Comment.count.must_equal 2
+    post = Post.first
+    post.title.must_equal("Very Cool Post")
+    post.author.must_equal("John Doe")
+    comments = post.comments
+    comments.map(&:message).must_equal(["Great post MAN!", "Really?"])
+    comments.map(&:author).must_equal(["Mr. Smith", "Vasya"])
   end
 end
